55dcbb253f02c6fac59a51028e5530b191c97421
小野裕輔 <y-ono@nswt.co.jp>
2022-08-12 10:08:24 +0900

    First commit

---
+++ Csv2DataGridView/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Csv2DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    csvFilePath.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TextFieldParser parser = new TextFieldParser(csvFilePath.Text, Encoding.GetEncoding("Shift_JIS"))
+            {
+                TextFieldType = FieldType.Delimited
+            };
+            parser.SetDelimiters(","); // 区切り文字はコンマ
+            string[] colheaders = parser.ReadFields(); // 1行読み込み
+
+            while (!parser.EndOfData)
+            {
+                string[] row = parser.ReadFields(); // 1行読み込み
+                dataGridView1.Rows.Add(row);
+            }
+        }
+    }
+}
