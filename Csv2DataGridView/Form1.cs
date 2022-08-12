6e9bbcebce41cae1f7466be1d859692ecf5a941c
小野裕輔 <y-ono@nswt.co.jp>
2022-08-12 12:58:31 +0900

    Add error check

--- Csv2DataGridView/Form1.cs
+++ Csv2DataGridView/Form1.cs
@@ -14,6 +14,8 @@ namespace Csv2DataGridView
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            dataGridView1.Rows.Clear(); // DGV初期化
+
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
@@ -37,11 +39,63 @@ namespace Csv2DataGridView
             };
             parser.SetDelimiters(","); // 区切り文字はコンマ
 
+            int i = 0;  // CSV行カウント
+            bool errorFlg = false;
             while (!parser.EndOfData)
             {
                 string[] row = parser.ReadFields(); // 1行読み込み
                 dataGridView1.Rows.Add(row);
+
+                // CSVエラーチェック（列ごとにチェック）
+                for(int j = 0; j < row.Length; j++)
+                {
+                    // 1列目のチェック
+                    if (j == 0)
+                    {
+                        // 必須であること
+                        if (String.IsNullOrEmpty(row[j])){
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Yellow;
+                            errorFlg = true;
+                        }
+                        // 数値であること
+                        if (!int.TryParse(row[j], out int num))
+                        {
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Yellow;
+                            errorFlg = true;
+                        }
+                    }
+                    // 2列目のチェック
+                    if (j == 1)
+                    {
+                        // 必須チェック
+                        if (String.IsNullOrEmpty(row[j]))
+                        {
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Yellow;
+                            errorFlg = true;
+                        }
+                    }
+                    // 14列目のチェック
+                    if (j == 13)
+                    {
+                        // 数値5桁であること
+                        if (row[j].ToString().Length != 5)
+                        {
+                            dataGridView1.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Yellow;
+                            errorFlg = true;
+                        }
+                    }
+                }
+                i++;
             }
+
+            if (errorFlg == true)
+            {
+                MessageBox.Show("Error!");
+                return;
+            }
+
+            MessageBox.Show("OK!");
+            return;
         }
     }
 }
